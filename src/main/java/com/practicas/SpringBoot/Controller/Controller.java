12b416e59d7a97f319d5e3c4a8b72a05a4dd021c
xitoar <xitoar@hotmail.com>
2022-04-11 19:45:54 -0300

    agregado de clase model Persona

--- src/main/java/com/practicas/SpringBoot/Controller/Controller.java
+++ src/main/java/com/practicas/SpringBoot/Controller/Controller.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.practicas.SpringBoot.Controller;
 
-/**
- *
- * @author xitoa
- */
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
     
 }
